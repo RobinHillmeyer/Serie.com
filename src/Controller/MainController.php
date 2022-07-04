2a5ba64947db0505a141a71ad8752f4be6526dfd
RobinHillmeyer <robin.hillmeyer13@gmail.com>
2022-07-04 10:13:34 +0200

    Twig et template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,8 +12,7 @@ class MainController extends AbstractController
      */
     public function home()
     {
-        echo "coucou";
-        die();
+       return $this->render('main/home.html.twig');
     }
 
     /**
@@ -21,8 +20,7 @@ class MainController extends AbstractController
      */
     public function test()
     {
-        echo "testounet";
-        die();
+        return $this->render('main/test.html.twig');
     }
 
 
